2adea9af5fe68fbb6ae6c3cf9a07ce43d03b7377
d1sc0 <ilovediscostu@gmail.com>
2023-08-11 23:27:48 +0100

    remove eroneous function

--- src/classes/parkrun.js
+++ src/classes/parkrun.js
@@ -270,17 +270,6 @@ class Parkrun {
     return output;
   }
 
-  async getAthleteDetails(athleteID) {
-    // /events/12/rosters/date
-    const res = await this._getAuthedNet()
-      .get(`/v1/athletes/${athleteID}`)
-      .catch(err => {
-        throw new NetError(err);
-      });
-
-    return new User(res.data, this);
-  }
-
   /**
    * Asynchronously get an event based on its ID.
    *
